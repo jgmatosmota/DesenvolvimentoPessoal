778f8c8fda2724998085fd905d70d777e611f490
jgmatosmota <jose.mota@sptech.school>
2022-11-02 02:00:32 -0300

    Criação de modal e alterações no CSS

---
+++ Projeto pessoal/Funções.js
@@ -0,0 +1,19 @@
+
+
+function loginCam(){
+    window.location.assign("login.html")
+
+}
+
+function cadastroCam(){
+    window.location.assign("cadastro.html")
+}
+
+function historiaCam(){
+    window.location.assign("historia.html")
+}
+
+function mostrarHistoria(){
+    papiroH.style.display="block"
+
+}
