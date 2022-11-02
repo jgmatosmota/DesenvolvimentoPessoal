04bd069efbf2f164f5b45787ab7ab1327b3afd5c
jgmatosmota <jose.mota@sptech.school>
2022-11-02 16:52:35 -0300

    Personalização de ícones e lógica por trás do storytelling

--- Projeto pessoal/Funções.js
+++ Projeto pessoal/Funções.js
@@ -12,8 +12,35 @@ function cadastroCam(){
 function historiaCam(){
     window.location.assign("historia.html")
 }
+function jogoCam(){
+    window.location.assign("jogo.html")
+}
 
 function mostrarHistoria(){
     papiroH.style.display="block"
+   if(contador==0) {contador++}
+}
 
+function voltarHist(){
+        
+    papiroH.style.display="none"
+    quest2.style.display="block"
+
+
+}
+function mostrarHistoria1(){
+    papiroH.style.display="block"
+    quest3.style.display="block"
+   if(contador==1){contador++}
+   
+}
+function mostrarHistoria2(){
+    papiroH.style.display="block"
+   if(contador==2){contador++}
+   quest4.style.display="block"
+   
+}
+function mostrarHistoria3(){
+    papiroH.style.display="block"
+   if(contador==3){contador++}
 }
